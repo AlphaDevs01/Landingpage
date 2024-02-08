630ed3bd38084d4547cac192ecafc897fff50904
Diomar Gonçalves <50972479+DiomarBR@users.noreply.github.com>
2024-02-08 08:04:42 -0300

    up

---
+++ tatoo.py
@@ -0,0 +1,9 @@
+def life_love_family():
+    life = {
+        "dreams": ["a prosperous future", "a thriving family", "unconditional love"],
+        "goals": ["strong relationships", "achieving goals", "pursuit of happiness"],
+        "relationships": ["friendship", "familial", "conjugal"],
+        "assessment": "family's love is the true wealth in life"
+    }
+    return life["assessment"]
+print(life_love_family())
